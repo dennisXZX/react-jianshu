bbd5dfb9d14d37b955c236eb641403b235a14ef9
Dennis Xiao <dennisboys@gmail.com>
2018-12-30 15:19:08 +1000

    set up styled-components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
+import ResetStyle from './styles/resetStyle'
+import GlobalStyle from './styles/globalStyle'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div>
+        <ResetStyle />
+        <GlobalStyle />
         jianshu
       </div>
     );
