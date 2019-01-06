bef495aa38e7644d2fbabd51ba4c100d1e4a230f
Dennis Xiao <dennisboys@gmail.com>
2019-01-06 13:04:37 +1000

    add detail page layout

--- src/pages/detail/DetailPage.js
+++ src/pages/detail/DetailPage.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react'
+import {
+  Content,
+  DetailWrapper,
+  Header
+} from './detailPageStyle'
 
 class Detail extends Component {
   render () {
     return (
-      <div>detail</div>
+      <DetailWrapper>
+        <Header>Title</Header>
+        <Content>
+          <p><b>Lorem ipsum dolor sit amet</b>, consectetur adipisicing elit. Autem consequuntur delectus deleniti doloribus eos incidunt ipsa laboriosam magnam maxime, modi pariatur quae repudiandae sapiente, sequi sint suscipit tempore? Aperiam, minima?</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem consequuntur delectus deleniti doloribus eos incidunt ipsa laboriosam magnam maxime, modi pariatur quae repudiandae sapiente, sequi sint suscipit tempore? Aperiam, minima?</p>
+        </Content>
+      </DetailWrapper>
     )
   }
 }
